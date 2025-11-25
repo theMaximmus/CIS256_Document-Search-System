974c446e70e5c35936d21969c916f2ba9efc6868
maksymstesev <m.stesev@icloud.com>
2025-11-24 22:26:12 -0800

    Finalized ArrayStack.java class.

--- src/main/java/csm/cis256/ArrayStack.java
+++ src/main/java/csm/cis256/ArrayStack.java
@@ -1,6 +1,7 @@
 package csm.cis256;
 
 import java.util.Arrays;
+import java.util.NoSuchElementException;
 
 /**
  * A generic Stack implementation using an array.
@@ -19,26 +20,72 @@ public class ArrayStack<T> {
         this.size = 0;
     }
 
+    /**
+     * Pushes an element onto the top of the stack.
+     * Resizes the underlying array if capacity is reached.
+     * @param element The element to add.
+     */
     public void push(T element) {
+        if (size == data.length) {
+            resize();
+        }
 
+        data[size] = element;
+        size++;
     }
 
+    /**
+     * Removes and returns the element at the top of the stack.
+     * @return The element removed from the top.
+     * @throws NoSuchElementException if the stack is empty.
+     */
     public T pop() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException("Cannot pop from an empty stack.");
+        }
+
+        // Get the item at the top (index is size - 1)
+        T element = data[size - 1];
+
+        // Avoid memory leak: nullify the reference so GarbageCollector can recycle it
+        data[size - 1] == null;
+
+        size--;
+
+        return element;
     }
 
+    /**
+     * Returns the element at the top of the stack without removing it.
+     * @return The element at the top.
+     * @throws NoSuchElementException if the stack is empty.
+     */
     public T top() {
-        return data[size -1 ];
+        if (isEmpty()) {
+            throw new NoSuchElementException("Cannot access top of an empty stack.");
+        }
+        return data[size - 1];
     }
 
+    /**
+     * Returns the number of elements currently in the stack.
+     * @return The integer size.
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * Checks of the stack is empty.
+     * @return true if size is 0. false otherwise.
+     */
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /**
+     * Helper method to double the array capacity when full.
+     */
     private void resize() {
         data = Arrays.copyOf(data, data.length * 2);
     }
