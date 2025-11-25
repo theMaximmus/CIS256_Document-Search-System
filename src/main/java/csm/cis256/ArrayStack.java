d02acb2db1806f0f91cb9aed6e8a7909981c7403
maksymstesev <m.stesev@icloud.com>
2025-11-24 20:23:23 -0800

    Preliminary body of ArrayStack.java class.

--- src/main/java/csm/cis256/ArrayStack.java
+++ src/main/java/csm/cis256/ArrayStack.java
@@ -1,5 +1,45 @@
 package csm.cis256;
 
-public class ArrayStack {
-    
+import java.util.Arrays;
+
+/**
+ * A generic Stack implementation using an array.
+ * @param <T>
+ */
+public class ArrayStack<T> {
+    private T data[];
+    private int size;
+    private static final int INITIAL_CAPACITY = 10;
+
+    /**
+     * Constructor initializes the array with a default capacity.
+     */
+    public ArrayStack() {
+        this.data = (T[]) new Object[INITIAL_CAPACITY];
+        this.size = 0;
+    }
+
+    public void push(T element) {
+
+    }
+
+    public T pop() {
+        return null;
+    }
+
+    public T top() {
+        return data[size -1 ];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void resize() {
+        data = Arrays.copyOf(data, data.length * 2);
+    }
 }
